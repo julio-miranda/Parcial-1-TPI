4f98dd42d76d85baded39a1d5108fe5347f07765
GR20036 <billy.kevin2001@gmail.com>
2022-09-01 11:01:12 -0600

    js

--- Parcial_1.js
+++ Parcial_1.js
@@ -1,29 +1,54 @@
-const producto = (id,stock,nombre,precio) => {
+function Indumentaria (id,stock,nombre,precio) {
     this.id = id;
     this.stock = stock;
-    tihs.nombre = nombre;
+    this.nombre = nombre;
     this.precio = precio;
 }
 
+
+function Camisa(id,stock,nombre,precio){
+    Indumentaria.call(this,id,stock,nombre,precio);
+}
+function Pantalon(id,stock,nombre,precio){
+    Indumentaria.call(this,id,stock,nombre,precio);
+}
+function Zapatos(id,stock,nombre,precio){
+    Indumentaria.call(this,id,stock,nombre,precio);
+}
+Camisa.prototype = Object.create(Indumentaria.prototype);
+Camisa.prototype.constructor = Camisa;
+Pantalon.prototype = Object.create(Indumentaria.prototype);
+Pantalon.prototype.constructor = Pantalon;
+Zapatos.prototype = Object.create(Indumentaria.prototype);
+Zapatos.prototype.constructor = Zapatos;
+
 const Tienda_Virtual = () => {
     const inventario = () => {
-        const Camisa = () => {
-        const  prototype = producto(1,2,"Camisa",10);
-        return Object.assign({},prototype,);
-        };
-        const Pantalon = () => {
-            const prototype = producto(2,2,"Pantalon",13);
-        return Object.assign({},prototype);
-        };
-        const Zapatos = () => {
-        const prototype = producto(3,2,"Zapatos",20);
-        return Object.assign({},prototype);
-        }
+        const camisa = new Camisa(1,2,"Camisa",10);
+        const pantalon = new Pantalon(2,2,"Pantalon",15);
+        const zapatos = new Zapatos(3,2,"Zapatos",30);
     }
-    const carritoDeCompras = () => {
-
-    }
+    const carritoDeCompras = [];
     const agregarProducto = (producto) => {
-
+            if(producto.stock > 0){
+                carritoDeCompras.push(producto);
+                prompt("El producto ha sido agregado correctamente al carrito");
+            }
+            else{
+                prompt("Lo sentimos, ya no tenemos existencias de ese producto");
+            }
+    }
+    const realizarPago = () => {
+        actualizar_inventario();
+        let total = 0;
+        carritoDeCompras.forEach(P => {
+            total += P.precio;
+        });
+        prompt(`El total de su compra es: ${total}, Gracias por su compra`);
+    }
+    const actualizar_inventario = () => {
+        inventario.forEach(p => {
+            prompt("Inventario actualizado");
+        });
     }
 }
