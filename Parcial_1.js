5caef53a51608e47e3f5b0fd4ee107e9fcb824d0
GR20036 <billy.kevin2001@gmail.com>
2022-09-01 09:50:39 -0600

    Archivo js inicial

---
+++ Parcial_1.js
@@ -0,0 +1,29 @@
+const producto = (id,stock,nombre,precio) => {
+    this.id = id;
+    this.stock = stock;
+    tihs.nombre = nombre;
+    this.precio = precio;
+}
+
+const Tienda_Virtual = () => {
+    const inventario = () => {
+        const Camisa = () => {
+        const  prototype = producto(1,2,"Camisa",10);
+        return Object.assign({},prototype,);
+        };
+        const Pantalon = () => {
+            const prototype = producto(2,2,"Pantalon",13);
+        return Object.assign({},prototype);
+        };
+        const Zapatos = () => {
+        const prototype = producto(3,2,"Zapatos",20);
+        return Object.assign({},prototype);
+        }
+    }
+    const carritoDeCompras = () => {
+
+    }
+    const agregarProducto = (producto) => {
+
+    }
+}
